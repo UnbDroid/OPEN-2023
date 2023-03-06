124446813f4da99ec1a59b7f8d0d1dbd380dacd1
joshuaazev <gustavo.joshua81@gmail.com>
2023-03-05 22:04:33 -0300

    mudanças arthur

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,23 @@
 #include<Arduino.h>
 #include<Wire.h>
-#define Status1 0x02
-    #define mpu 0x68
-    #define USER_CTRL_AD 0x6A
-    #define INT_BYPASS_CONFIG_AD 0x37
-    #define mag 0x0C
-        #define CNTL1_AD 0x0A   
-    // Actually we don't need this step cause the reset value of the register 106 is 0x00
-    #define ASAX_AD 0x10  
-     #define DATA_READY_MASK 0x01
-    #define MAGIC_OVERFLOW_MASK 0x8
+
+#define eca 2
+
+#define ecb 3
+
+
+void meudeus(){
+
+    Serial.println("a");
+    
+}
 void setup(){
-    Wire.begin();
-    Wire.beginTransmission(mpu);
-    Wire.write(USER_CTRL_AD);
-    Wire.write(0x00);
-    Wire.endTransmission();
-    Wire.beginTransmission(mpu);
-    Wire.write(INT_BYPASS_CONFIG_AD);
-    Wire.write(0x02);   //0000 0010 in binary, turn on the bypass multiplexer
-    Wire.endTransmission();
-    Wire.beginTransmission(mag);
-    Wire.write(CNTL1_AD);
-    Wire.write(0x1F);
+    Serial.begin(9600);
+    pinMode(7, INPUT);
+    pinMode(6, INPUT);
     delay(100);
-    Wire.beginTransmission(mag);
-    Wire.write(CNTL1_AD);
-    Wire.write(0x00);
-    //wait for the mode changes
-    delay(100);
+    attachInterrupt(digitalPinToInterrupt(ecb), meudeus, RISING);
 
-    Wire.beginTransmission(mag);
-    Wire.write(CNTL1_AD);
-    Wire.write(0x16);   //0001 0110 in binary
-
-    //wait for the mode changes
-    delay(100);
 }
-
 void loop(){
-  Wire.beginTransmission(mag);
-        Wire.write(0x03);
-        Wire.requestFrom(mag,7,true);
-        float x = Wire.read()<<8|Wire.read();
-        Serial.println(x);
-        
-    
 }
-
-
-
-    
--- test/testeMag.cpp
+++ test/testeMag.cpp
@@ -1,35 +1,72 @@
-#include <Arduino.h>
-#include <Wire.h>
-#include <ColorSensor.h>
-#include <MotorDC.h>
-#include <Ultrassonic.h>
+#include<Arduino.h>
+#include<Wire.h>
 
-#define MPU_ADD 0x68
-#define USER_CRTL_add 0x6A
-#define INT_BYPASS_CONFIG_AD 0x37
-float gy,gx,gz;
+#define mpu9250_addr 0x68 //endereço MPU
+#define magn_addr 0x0C //Endereço do magnetometro 
+#define PWR_MGMT_1 0x6b //registrador de configuração de energia
+#define PWR_MGMT_2 0x6c //registrador de configuração de energia
+#define INT_PIN_CFG 0x37
+#define CONFIG 0x1a
+#define SMPLRT_DIV 0x19
+#define CNTL1 0x0a
+#define MAG_XOUT_L 0x03
 void setup(){
-  Serial.begin(9600);
+    Serial.begin(9600);
   Wire.begin();
-  Wire.beginTransmission(MPU_ADD);
-  Wire.write(USER_CRTL_add);
-  Wire.write(0x00, true);
-  
-}
-void loop(){
-    Wire.beginTransmission(gyroAdress);
-    Wire.write(0x03);
-    Wire.endTransmission(false);
-    Wire.requestFrom(gyroAdress,6, true);
-    gx = float(Wire.read()<<8 | Wire.read())/0.6;
-    gy = Wire.read()<<8 | Wire.read();
-    gz = Wire.read()<<8 | Wire.read();
-    Serial.print("Valor em X ");
-    Serial.print(gx);
-    Serial.print(" Valor em y ");
-    Serial.println(gy);
-    Serial.print(" Valor em z ");
-    Serial.println(gz);
-    delay(400);
+  //  Reseta os valores dos registradores 
+  Wire.beginTransmission(mpu9250_addr);
+  Wire.write(PWR_MGMT_1);
+  Wire.write(0x80);
+  Wire.endTransmission();
+  //
+  //desabilita o sleep seta o clock automaticamente
+  Wire.beginTransmission(mpu9250_addr);
+  Wire.write(PWR_MGMT_1);
+  Wire.write(0x01);
+  Wire.endTransmission();
+  //
+  //
+  Wire.endTransmission(mpu9250_addr);
+  Wire.write(INT_PIN_CFG);
+  Wire.write(0x02);
+  Wire.endTransmission();
+  //
+  // Setando o modo de energia do magnetometro
+  Wire.beginTransmission(magn_addr);
+  Wire.write(PWR_MGMT_2);
+  Wire.write(0x00);
+  Wire.endTransmission();
+  //
+  // Seta a configuração dos registradores
+  Wire.beginTransmission(mpu9250_addr);
+  Wire.write(CONFIG);
+  Wire.write(0x03);
+  Wire.endTransmission();
+  //
 
+  Wire.beginTransmission(mpu9250_addr);
+  Wire.write(SMPLRT_DIV);
+  Wire.write(0x00);
+  Wire.endTransmission();
+
+  Wire.beginTransmission(magn_addr);
+  Wire.write(CNTL1);
+  Wire.write(0x16);
+  Wire.endTransmission();
+  //
+}
+#define a1 0x02;
+#define a2 0x01;
+float magx, magy,magz;
+void loop(){
+  Wire.beginTransmission(magn_addr);
+  Wire.write(MAG_XOUT_L);  //de onde eu quero começar a receber os dados 
+  Wire.endTransmission();
+  Wire.requestFrom(mpu9250_addr, 6, true);
+  magx = Wire.read()<<8 | Wire.read();
+  magy = Wire.read()<<8 | Wire.read();
+  magz = Wire.read()<<8 | Wire.read();
+
+  Serial.println(magx);
+  delay(1000);
 }
