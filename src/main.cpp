686b3689818ae712d77ffafe1f89f6dcd970ea5f
Arthur Neves <arthurneves0512@gmail.com>
2023-03-08 17:19:31 -0300

    codigo de teste do mag

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "MPU9250.h"
 #include<Arduino.h>
+#include<math.h>
 MPU9250 mpu; // You can also use MPU9255 as is
 
 void setup() {
@@ -8,10 +9,25 @@ void setup() {
     delay(2000);
 
     mpu.setup(0x68);  // change to your own address
+    mpu.setMagneticDeclination(21);
 }
+
 
 void loop() {
     if (mpu.update()) {
-        Serial.println(mpu.getMagX());
+        float x = pow(mpu.getMagX(),2);
+        float y = pow(mpu.getMagY(),2);
+        float z = pow(mpu.getMagZ(),2);
+        float result = x+y+z;
+        float B = sqrt(result);
+        float anguloInclinacao = asin(-mpu.getMagY()/B);
+        float roll = atan2(mpu.getMagX(),mpu.getMagY());
+        float angulo = (roll *180)/PI;
+        if(angulo>40){
+            Serial.println(angulo*2);
+        }
+        else{
+            Serial.println(angulo*0.4);
+        }
     }
 }
