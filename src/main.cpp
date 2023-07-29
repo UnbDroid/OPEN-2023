4e919b39d99bdc89837eb43476204cf4429e2b57
Arthur <arthurneves0512@gmail.com>
2023-07-29 11:13:16 -0300

    mudei função de teste

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,11 @@
 #include"LightSensor.h"
 #include<Pins.h>
 
-LightSensor sensorDireita(A13);
-LightSensor sensorEsquerda(A14);
 void setup(){
     Serial.begin(9600);
-
 }
 
 void loop(){
-    sensorEsquerda.read();
-    sensorDireita.read();
-    if(sensorEsquerda.getCrossed()){
-        Serial.println("Esquerda Atravessou");
-    }
-    else if(sensorDireita.getCrossed()){
-        Serial.println("Direita Atravessou");
-    }
+    
 }
 
