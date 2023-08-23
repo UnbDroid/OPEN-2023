086cdb71b1acb35d5309570a0aadb0b2c9b09bd8
LuisaFerro <luisaferro9@gmail.com>
2023-08-23 13:23:31 -0300

    reto e giro PID

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,6 @@
 
 MotorDC LeftMotor(M_LEFT_LPWM, M_LEFT_RPWM,ENC_A_LEFT,ENC_B_LEFT);
 MotorDC RightMotor(M_RIGHT_LPWM, M_RIGHT_RPWM,ENC_A_RIGHT,ENC_B_RIGHT);
-LightSensor LeftLightSensor(A0_ESQUERDA_FRENTE);
-LightSensor RightLightSensor(A0_ESQUERDA_FRENTE);
-
 
 void TakeMemoryLeftMotor(){ // fica na main
   LeftMotor.readEncoder();
@@ -24,51 +21,14 @@ void setup()
   attachInterrupt(digitalPinToInterrupt(ENC_B_RIGHT), TakeMemoryRightMotor, RISING); //deixa na main
   attachInterrupt(digitalPinToInterrupt(ENC_B_LEFT),TakeMemoryLeftMotor, RISING); // deixa na main
 
-  // movePID(FORWARD, 150 ,&LeftMotor, &RightMotor);
-  // rotates90(LEFT,150,&LeftMotor, &RightMotor);
-
-  // int count = 135;
-  // for (int i = 0; i < count; i++)
-  // {
-  //   LeftMotor.moveForward(i);
-  //   RightMotor.moveForward(i);
-  //   Serial.println(i);
-  // }
-  // stop(&LeftMotor,&RightMotor);
-  
-  // delay(1000);
-  // rotates90(LEFT,150,&LeftMotor, &RightMotor);
-  // rotates90(LEFT,150,&LeftMotor, &RightMotor);
-  // delay(1000);
 } 
 
 void loop(){
-  movePID(FORWARD, 130 ,&LeftMotor, &RightMotor);  
-
-  // LeftMotor.moveForward(130);
-  // RightMotor.moveForward(130);
-
-  // while (analogRead(A0_ESQUERDA_FRENTE) < 100 && analogRead(A0_DIREITA_FRENTE) < 100)
-  // {
-  //   movePID(FORWARD, 130 ,&LeftMotor, &RightMotor);  
-  // }
-  
-  // movePID(FORWARD, 150 ,&LeftMotor, &RightMotor);
-  // stop(&LeftMotor,&RightMotor);
-
-
-  // Serial.print(analogRead(A0_ESQUERDA_FRENTE));
-  // Serial.print(" ");
-  // Serial.print(analogRead(A0_DIREITA_FRENTE));
-  // Serial.println();
-
-
 
   // Usando o serial, podemos ver o que os encoders estão retornando:
-  Serial.print(LeftMotor.getEncoder());
-  Serial.print(" ");
-  Serial.print(RightMotor.getEncoder());
-  Serial.println();
-
+  // Serial.print(LeftMotor.getEncoder());
+  // Serial.print(" ");
+  // Serial.print(RightMotor.getEncoder());
+  // Serial.println();
 
 }
